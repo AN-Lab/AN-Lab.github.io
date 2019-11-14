0b880a6df512534bd3e5e48f18a0f44bace2ae16
Eric <mcr.988899@163.com>
2019-11-14 19:26:37 +0800

    mdeditor

--- mdeditor/mdeditor.js
+++ mdeditor/mdeditor.js
@@ -3,13 +3,15 @@ $(function () {
 
   $("#catalog-container").css("height", height + "px");
 
+  $("#editor").load("help.md");
+
   var editor = editormd("editor", {
     placeholder: '本编辑器支持Markdown编辑，左边编写，右边预览',  //默认显示的文字
     width: "100%",
     height: height - 2,
     theme: "light",//工具栏主题
     previewTheme: "light",//预览主题
-    editorTheme: "solarized",//编辑主题
+    editorTheme: "xq-light",//编辑主题
     emoji: false,
     //markdown: "",     // dynamic set Markdown text
     path: "editor.md/lib/",  // Autoload modules mode, codemirror, marked... dependents libs path
@@ -19,7 +21,7 @@ $(function () {
     sequenceDiagram: true,       // 开启时序/序列图支持，默认关闭,
     toolbarIcons: function () {  //自定义工具栏，后面有详细介绍
       //return editormd.toolbarModes['full']; // full, simple, mini 三种默认模式
-      return ["undo", "redo", "|", "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|", "h1", "h2", "h3", "h4", "h5", "h6", "|", "list-ul", "list-ol", "hr", "|", "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "pagebreak", "|", "goto-line", "watch", "preview", "fullscreen", "clear", "search", "|", "help", "info"]; //自定义工具栏
+      return ["undo", "redo", "|", "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|", "h1", "h2", "h3", "h4", "h5", "h6", "|", "list-ul", "list-ol", "hr", "|", "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "pagebreak", "|", "goto-line", "watch", "preview", "fullscreen", "clear", "search", "|", "help"]; //自定义工具栏
     }
   });
 
@@ -28,7 +30,24 @@ $(function () {
   if (avatar_url != "" && avatar_url != null) {
     $("#avatar-container img").attr("src", avatar_url);
   }
+
+  // $("input.tag").bind('input propertychange',function(){
+  //   $(this).width(getTagWidth($(this).val()));
+  // });
+
+  // $(".tag-box .deleteButton").click(function(){
+  //   alert("sa");
+  //   $(this).remove;
+  // })
 })
+
+function openDetails(){
+  $("#details").css("display","block");
+}
+
+function closeDetails(){
+  $("#details").css("display","none");
+}
 
 function getMd() {
   console.log(editor.getMarkdown());
@@ -53,4 +72,49 @@ function getCookie(cname) {
     }
   }
   return "";
+}
+
+//获取字符串在前端中的宽度
+function getTagWidth(text){
+  var sensor = $("<pre>"+text+"</pre>").css({display: 'none'});
+  $('body').append(sensor); 
+  var width = sensor.width();
+  sensor.remove();
+  return width;
+}
+
+//在location元素前方添加标签,并绑定事件
+function addTag(location){
+  var tagBox = $("input.tag");
+  console.log(tagBox.length+"----"+$("input.tag").eq(tagBox.length-1).val());
+  if (tagBox.length != 0 && $("input.tag").eq(tagBox.length-1).val()=="") return;
+  if (tagBox.length >= 5){
+    alert("当前标签数量已达上限！");
+    return;
+  }
+  $(location).before("<div class='tag-box'><input type='text' class='tag' onblur='checkTagRepe()'><div class='delete-button'>&times</div></div>");
+  $("input.tag").bind('input propertychange',function(){
+    $(this).width(getTagWidth($(this).val()+3));
+  });
+  $(".tag-box .delete-button").click(function(){
+    $(this).parent().remove();
+  })
+}
+
+//删除标签
+function deleteTag(thisTag){
+  thisTag.remove;
+}
+
+//检查当前标签是否存在重复，若存在则删除其中靠后的一个标签
+function checkTagRepe(){
+  var tags = document.getElementsByClassName("tag");
+  for (let i = 0; i < tags.length-1;i++){
+    for (let j = i+1;j<tags.length;j++){
+       if (tags[i].value==tags[j].value){
+         tags[j].parentNode.remove();
+         return;
+       }
+    }
+  }
 }
