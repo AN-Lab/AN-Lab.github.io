dedef30a8b258ebc8f9fbb4c298bacf495b9ef57
Eric <mcr.988899@163.com>
2019-11-16 19:46:19 +0800

    Update mdeditor.js

--- mdeditor/mdeditor.js
+++ mdeditor/mdeditor.js
@@ -1,9 +1,11 @@
+var editor;
+
 $(function () {
   var height = document.documentElement.clientHeight - 50; //编辑器和左侧目录的高度
 
   $("#catalog-container").css("height", height + "px");
 
-  var editor = editormd("editor", {
+  editor = editormd("editor", {
     placeholder: '本编辑器支持Markdown编辑，左边编写，右边预览',  //默认显示的文字
     width: "100%",
     height: height - 2,
@@ -39,12 +41,12 @@ $(function () {
   // })
 })
 
-function openDetails(){
-  $("#details").css("display","block");
+function openDetails() {
+  $("#details").css("display", "block");
 }
 
-function closeDetails(){
-  $("#details").css("display","none");
+function closeDetails() {
+  $("#details").css("display", "none");
 }
 
 function getMd() {
@@ -56,16 +58,16 @@ function insertText() {
 }
 
 //发布文章
-function submit(){
+function submit() {
   var title = $("#title").val();
   var content = editor.getMarkdown();
   var tag_box = document.getElementsByClassName("tag");
   var tags = "";
-  for (let i = 0;i<tag_box.length;i++){
-    tags+=tag_box[i].value+", ";
+  for (let i = 0; i < tag_box.length; i++) {
+    tags += tag_box[i].value + ", ";
   }
   var category = $("#category-container option:selected");
-  console.log("title:"+title+"\ncontent:"+content+"\ntags:"+tags+"\ncategory:"+category);
+  console.log("title:" + title + "\ncontent:" + content + "\ntags:" + tags + "\ncategory:" + category);
 }
 
 //获取cookie信息
@@ -86,46 +88,46 @@ function getCookie(cname) {
 }
 
 //获取字符串在前端中的宽度
-function getTagWidth(text){
-  var sensor = $("<pre>"+text+"</pre>").css({display: 'none'});
-  $('body').append(sensor); 
+function getTagWidth(text) {
+  var sensor = $("<pre>" + text + "</pre>").css({ display: 'none' });
+  $('body').append(sensor);
   var width = sensor.width();
   sensor.remove();
   return width;
 }
 
 //在location元素前方添加标签,并绑定事件
-function addTag(location){
+function addTag(location) {
   var tagBox = $("input.tag");
-  console.log(tagBox.length+"----"+$("input.tag").eq(tagBox.length-1).val());
-  if (tagBox.length != 0 && $("input.tag").eq(tagBox.length-1).val()=="") return;
-  if (tagBox.length >= 5){
+  console.log(tagBox.length + "----" + $("input.tag").eq(tagBox.length - 1).val());
+  if (tagBox.length != 0 && $("input.tag").eq(tagBox.length - 1).val() == "") return;
+  if (tagBox.length >= 5) {
     alert("当前标签数量已达上限！");
     return;
   }
   $(location).before("<div class='tag-box'><input type='text' class='tag' onblur='checkTagRepe()'><div class='delete-button'>&times</div></div>");
-  $("input.tag").bind('input propertychange',function(){
-    $(this).width(getTagWidth($(this).val()+3));
+  $("input.tag").bind('input propertychange', function () {
+    $(this).width(getTagWidth($(this).val() + 3));
   });
-  $(".tag-box .delete-button").click(function(){
+  $(".tag-box .delete-button").click(function () {
     $(this).parent().remove();
   })
 }
 
 //删除标签
-function deleteTag(thisTag){
+function deleteTag(thisTag) {
   thisTag.remove;
 }
 
 //检查当前标签是否存在重复，若存在则删除其中靠后的一个标签
-function checkTagRepe(){
+function checkTagRepe() {
   var tags = document.getElementsByClassName("tag");
-  for (let i = 0; i < tags.length-1;i++){
-    for (let j = i+1;j<tags.length;j++){
-       if (tags[i].value==tags[j].value){
-         tags[j].parentNode.remove();
-         return;
-       }
+  for (let i = 0; i < tags.length - 1; i++) {
+    for (let j = i + 1; j < tags.length; j++) {
+      if (tags[i].value == tags[j].value) {
+        tags[j].parentNode.remove();
+        return;
+      }
     }
   }
 }
